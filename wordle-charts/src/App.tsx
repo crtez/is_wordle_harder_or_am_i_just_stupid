9e37ef06a68814a6c81c18da32944b487e5b1e14
crtez <27513125+crtez@users.noreply.github.com>
2024-12-03 16:28:51 -0700

    fixed hard mode toggle in wrong spot

--- wordle-charts/src/App.tsx
+++ wordle-charts/src/App.tsx
@@ -373,7 +373,7 @@ const WordleChart = () => {
   return (
     <div className="h-[100dvh] p-4 flex flex-col overflow-hidden">
       <div className="grid grid-cols-12 gap-3 mb-4">
-        <div className="col-span-10 grid grid-cols-6 gap-3 items-start">
+        <div className="col-span-10 grid grid-cols-6 gap-3 items-center">
           <Select 
             defaultValue="standard" 
             onValueChange={handleModeChange}
