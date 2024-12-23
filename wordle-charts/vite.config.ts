0d647d06e52f785cc53bf3e3176ede5152849672
crtez <27513125+crtez@users.noreply.github.com>
2024-12-23 10:47:30 -0700

    visualizer (#15)

--- wordle-charts/vite.config.ts
+++ wordle-charts/vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   base: '/is_wordle_harder_or_am_i_just_stupid/',
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
